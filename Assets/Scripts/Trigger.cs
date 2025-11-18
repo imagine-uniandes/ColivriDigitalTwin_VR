934ce52f90a5be82f5229662ea3d58472647e5b6
Carla González Mina <158210542+Carlixgonzam@users.noreply.github.com>
2025-11-18 12:28:48 -0500

    Ontriggercorregido

---
+++ Assets/Scripts/Trigger.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Trigger : MonoBehaviour
+{
+    [SerializeField] UnityEvent onTriggerEnter;
+    [SerializeField] UnityEvent onTriggerExit;
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            onTriggerEnter.Invoke();
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            onTriggerExit.Invoke();
+        }
+    }
+}
