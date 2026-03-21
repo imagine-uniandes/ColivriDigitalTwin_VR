9290ceb395ae19ee0216017039d351225b83364b
jdonatog39 <j.donatog@uniandes.edu.co>
2026-03-21 18:10:22 -0500

    cambios definitivos

---
+++ Assets/Scripts/triggerBoxAura.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class triggerBoxAura : MonoBehaviour
+{
+    [SerializeField] UnityEvent onTriggerEnter;
+    [SerializeField] UnityEvent onTriggerExit;
+
+    private void Start()
+    {
+        print("Trigger script is active en" + gameObject.name);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            onTriggerEnter.Invoke();
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            onTriggerExit.Invoke();
+        }
+    }
+}
